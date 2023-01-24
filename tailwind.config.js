d0e84a13253c0255795a91cce158c04368adb78f
Rashadul Haque <rashadulhaque@Rashaduls-MacBook-Air.local>
2023-01-24 22:56:10 +0100

    updated with project structured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
-const { join } = require('path');
+const { join } = require("path");
 module.exports = {
   content: [
     join(
       __dirname,
-      '{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+      "{src,pages,components}/**/*!(*.stories|*.spec).{ts,tsx,html}"
     ),
-   
   ],
   theme: {
-    screen:{
-      
-    },
+    screen: {},
     extend: {
-      
+      colors: {
+        prime: "#00ff00",
+        brownee: "#8B5E3C",
+        main: "#3EB489",
+        background: "#FFFFFF",
+        txt: "#333333",
+        "light-background": "#F5F5F5",
+        light: "#E6F9E6",
+      },
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+        serif: ["Montserrat", "serif"],
+        mono: ["Raleway", "monospace"],
+      },
     },
   },
   plugins: [],
