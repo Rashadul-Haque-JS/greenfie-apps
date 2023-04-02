97be842991b01e60ceefabc7f9d5c90a2fbcde8f
Rashadul Haque <rashadulhaque@Rashaduls-MacBook-Air.local>
2023-04-02 21:27:18 +0200

    updated

--- src/pages/api/auth/updateForgetPassword.ts
+++ src/pages/api/auth/updateForgetPassword.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import Users from '@/server/models/users';
-import { compare, hash } from 'bcryptjs';
+import { hash } from 'bcryptjs';
 import bcrypt from "bcryptjs";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -8,10 +8,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.status(405).json({ error: 'Method Not Allowed' });
     return;
   }
-
-  const { email, oldPassword, newPassword } = req.body;
+  const { email, password } = req.body;
   // Check if email, old password, and new password are present in request body
-  if (!email || !oldPassword || !newPassword) {
+  if (!email || !password) {
     res.status(400).json({ error: 'Invalid Request' });
     return;
   }
@@ -19,20 +18,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const user = await Users.findOne(email);
   // Check if user exists
   if (!user) {
-    res.status(400).json({ error: 'Invalid Email or Password' });
-    return;
-  }
-  // Compare old password with hashed password in database
-  const passwordMatch = await compare(oldPassword, user.password);
-
-  if (!passwordMatch) {
-    res.status(400).json({ error: 'Invalid Email or Password' });
+    res.status(400).json({ error: 'Invalid email' });
     return;
   }
   // Hash new password
   const salt = await bcrypt.genSalt(12);
-  const hashedPassword = await hash(newPassword, salt);
+  const hashedPassword = await hash(password, salt);
   // Update user's password in database
   await user.updateOne({ password: hashedPassword });
-  res.status(200).json({ message: 'Password update successful' });
+  res.status(200).json({ message: 'Password reset successful' });
 }
