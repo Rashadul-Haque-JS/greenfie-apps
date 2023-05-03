39974c6d8f1e88e240802bfd4048a94fb15ad572
Rashadul Haque <rashadulhaque@Rashaduls-MacBook-Air.local>
2023-05-03 21:28:02 +0200

    updated

--- src/components/cards/recipie.tsx
+++ src/components/cards/recipie.tsx
@@ -5,18 +5,17 @@ const RecipeCard = ({ recipe }: GenericProps) => {
   return (
     <div className='max-w-sm rounded overflow-hidden shadow-lg m-10 h-[480px]'>
       <Image className='w-full' src={recipe.image} alt={recipe.title} width={500} height={500} />
-      <div className='px-6 py-4'>
-        <div className='font-bold text-xl mb-2'>{recipe.title}</div>
-        <p className='text-gray-700 text-base'>{recipe.description}</p>
+      <div className='px-6 py-4 flex flex-col justify-center items-center '>
+        <h1 className='font-bold text-xl mb-2 flex-shrink-0'>{recipe.title}</h1>
+        <p className='text-gray-700 text-base flex-shrink-0'>{recipe.description}</p>
       </div>
-      <div className='px-6 py-4'>
-        <Link href={`/recipes/${recipe._id}`} className='flex items-center bg-txt hover:bg-main text-white font-bold py-2 px-4 w-3/4 rounded'>
-
+      <div className='px-6 py-4 flex justify-center items-center'>
+        <Link href={`/recipes/${recipe._id}`} className='flex items-center justify-center bg-txt hover:bg-main text-white font-bold py-2 px-4 w-full rounded'>
           <span className='mx-2'>Watch Now</span>
           <i className="material-icons">play_circle_outline</i>
-
         </Link>
       </div>
+
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,16 @@ module.exports = {
         'lg': {'min': '992px', 'max': '1199.98px'},
         'xl': {'min': '1200px'},
       },
+      fontSize: {
+        'sm': '0.875rem', // 14px
+        'base': '1rem',   // 16px
+        'lg': '1.125rem', // 18px
+        'xl': '1.25rem',  // 20px
+        '2xl': '1.5rem',  // 24px
+        '3xl': '1.875rem', // 30px
+        '4xl': '2.25rem', // 36px
+        '5xl': '3rem',    // 48px
+      },
       
       colors: {
         brownee: "#c70e05",
