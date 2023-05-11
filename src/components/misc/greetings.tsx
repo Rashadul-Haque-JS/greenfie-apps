7cba0b1a0638a8ab70af51726b256d7250337187
Rashadul Haque <rashadulhaque@Rashaduls-MacBook-Air.local>
2023-05-11 22:24:37 +0200

    updated

--- src/components/misc/greetings.tsx
+++ src/components/misc/greetings.tsx
@@ -8,18 +8,15 @@ const Greetings = () => {
    dispatch(setSignup(true))
   };
   return (
-    <div className="bg-txt px-4 xs:p-4 sm:p-4 md:p-6 w-full">
-      <p className="text-lime-400 text-center text-xs md:text-base font-medium mb-4 md:mb-6">
-        WELCOME TO THE FIRST ROOF VEGETABLES WEBSITE IN BD
-      </p>
-      <div className="flex flex-col md:flex-row justify-center items-center gap-6">
-        <h1 className="text-background text-center text-2xl md:text-4xl font-bold mb-4 md:mb-0">
-          Eat Fresh and Buy Local
-        </h1>
+    <div className="bg-txt px-4 xs:py-6 sm:py-6 py-6 w-full">
+      <p className="text-lime-400 text-center text-xs font-medium mb-4 md:mb-6 uppercase">
+      Discover fresh and local produce with Greenfie, BD's first rooftop vegetable hub </p>
+      <div className="flex flex-col md:flex-row justify-center items-center gap-4">
+        <h1 className="text-background text-center text-2xl font-bold mb-4 md:mb-0">
+        Fresh veggies. Connect & Share. Grow Together</h1>
         <button onClick={handleOptReg}
           className="relative bg-main text-background rounded-full py-3 px-8 font-bold hover:bg-opacity-70 transition duration-300 ease-in-out"
         >
-          {" "}
           Sign Up Now
         </button>
       </div>
