1181dd33f7ee67408984042036b1a1c1a8b4cce0
Rashadul Haque <rashadulhaque@Rashaduls-MacBook-Air.local>
2023-05-16 09:55:12 +0200

    updated

--- src/components/misc/landings.tsx/hero.tsx
+++ src/components/misc/landings.tsx/hero.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="relative w-full h-[280px] sm:h-[360px] md:h-[480px] 2xl:h-[640px]">
+    <div className="relative w-full h-[280px] sm:h-[360px] md:h-[480px] lg:h-[600px] xl:h-[600px] 2xl:h-[600px]">
       <Image
         src="/images/products.png"
         alt="Hero image"
         className="object-cover w-full h-full"
         layout="fill"
       />
-      <div className="absolute bottom-0 w-full bg-gray-800 xs:bg-opacity-25 sm:bg-opacity-25 bg-opacity-80 px-4 xs:py-6 sm:py-6 py-6">
+      <div className="absolute bottom-0 w-full lg:h-1/3 xl:h-1/3 bg-gray-800 bg-opacity-25 px-4 xs:py-6 sm:py-6 py-6">
         <div className="flex flex-col md:flex-row justify-center items-center gap-4">
-          <h2 className="text-white text-center text-lg font-medium mb-4 md:mb-6 uppercase">
+          <h2 className="text-white text-center text-lg xl:text-xl lg:p-16 xl:p-16 font-medium mb-4 md:mb-6 uppercase">
             Discover fresh and local produce with Greenfie, BD's first rooftop
             vegetables hub
           </h2>
