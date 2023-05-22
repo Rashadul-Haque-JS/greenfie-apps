398571e0ea858aae95dce2d53223491a56c8ac4b
Rashadul Haque <rashadulhaque@Rashaduls-MacBook-Air.local>
2023-05-22 13:01:01 +0200

    updated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,56 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang="en">
       <Head>
-        <link href='https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css' rel='stylesheet' />
-        <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons'/>
-
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/favicon/android-chrome-512x512.png"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js' defer></script>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"
+          defer
+        ></script>
       </body>
     </Html>
-  )
+  );
 }
